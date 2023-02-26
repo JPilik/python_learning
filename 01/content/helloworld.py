3f7dfa6045a962b801ceec5a200ff5c8e9d4da10
Rostron <1562993734157004@mil>
2023-02-26 17:37:34 +0300

    Changed Text so it's easier to understand

--- 01/content/helloworld.py
+++ 01/content/helloworld.py
@@ -1,7 +1,8 @@
-'''A simple program that prints out Hello World.'''
+'''A simple program that prints out Hello World. This is the main function'''
 
 def helloworld():
     '''hello world method in python'''
+    #make changes here
 
 if __name__ == '__main__':
     helloworld()
