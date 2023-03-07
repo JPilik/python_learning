18c6fde3f976b9f1a36ebf522a1692e43d628354
Jeffrey Pilik <jeffreypilik@Jeffreys-MacBook-Pro.local>
2023-03-07 15:10:14 +0300

    added a new file to mocks

---
+++ 02/pilik/content/mocks/mock_element.py
@@ -0,0 +1,19 @@
+'''A Mock of an element in the pysimplegui framework'''
+
+class Element:
+    '''Element Class'''
+
+    def print(self, args,
+    end = None,
+    sep = None,
+    text_color = None,
+    background_color = None,
+    justification = None,
+    font = None,
+    colors = None,
+    t = None,
+    b = None,
+    c = None,
+    autoscroll = True):
+        '''Mocks a print funciton'''
+        pass
